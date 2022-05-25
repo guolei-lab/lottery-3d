7dde0ce4d2342384912ba68651cdd39d1abe3e0c
gzll <guolei@mkit.com.cn>
2022-05-25 11:29:59 +0800

    wip(*): init project

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // https://cli.vuejs.org/zh/config/#lintonsave
+    lintOnSave: 'error', // [true, false, 'error', 'warning', 'default']
+    publicPath: './', // 线上构建出来是相对路径在demo页才好展示
+    configureWebpack: config => {
+        // 线上允许debugger
+        if (config.mode === 'production') {
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = false;
+        }
+    },
+    chainWebpack: config => {
+        config.plugin('html').tap(args => {
+            args[0].title = '2022MKIT年终大抽奖';
+            return args;
+        });
+    },
+    devServer: {
+        port: 8080,
+        host: '',
+        disableHostCheck: true
+    }
+};
